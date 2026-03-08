fac6a90c1c43efe3e2ba9683edffdb25caf8154c
Jessica Mhanna <225629570+jessicamhanna@users.noreply.github.com>
2026-03-08 16:46:35 -0700

    File transformation to uppercase

---
+++ ex_07_01.py
@@ -0,0 +1,11 @@
+# Ensure mbox-short.txt is in your PycharmProjects folder
+try :
+    fh = open( 'mbox-short.txt' )
+    for line in fh :
+        # rstrip() removes the invisible newline character [cite: 595, 1107]
+        ly = line.rstrip()
+        print( ly.upper() )
+except :
+    # This prevents the program from crashing if the file is missing [cite: 752, 793]
+    print( 'File cannot be opened: mbox-short.txt' )
+    quit()
